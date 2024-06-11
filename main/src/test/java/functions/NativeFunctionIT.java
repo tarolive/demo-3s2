d0506bc3593fa58301409b031e3a85b385367245
Tarcisio Oliveira <84419122+tarolive@users.noreply.github.com>
2024-06-11 13:51:26 -0300

    Delete main directory

--- main/src/test/java/functions/NativeFunctionIT.java
+++
@@ -1,9 +0,0 @@
-package functions;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-public class NativeFunctionIT extends FunctionTest {
-
-    // Execute the same tests but in native mode.
-}
